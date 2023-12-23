047aecdf608567175cd78b94c077d7ea348935f6
jiacai2050 <dev@liujiacai.net>
2023-12-23 23:30:01 +0800

    add more strict args check

--- example.zig
+++ example.zig
@@ -2,24 +2,37 @@ const std = @import("std");
 const emacs = @import("emacs");
 usingnamespace emacs;
 
+pub const allocator = std.heap.c_allocator;
+
 // Emacs dynamic module entrypoint
 pub fn init(env: emacs.Env) c_int {
-    _ = env.define_fn(
-        "zig-func",
+    _ = env.defineFunc(
+        "zig-greeting",
         struct {
-            fn f(e: emacs.Env) emacs.Value {
-                return e.message("hello emacs from zig");
+            fn f(e: emacs.Env, username: []const u8) !emacs.Value {
+                defer allocator.free(username);
+
+                const greeting = try std.fmt.allocPrintZ(
+                    allocator,
+                    "hello {s}!",
+                    .{username},
+                );
+                defer allocator.free(greeting);
+
+                return e.message(greeting);
             }
         }.f,
+        .{ .doc_string = "greet written in Zig" },
     );
 
-    _ = env.define_fn(
+    _ = env.defineFunc(
         "zig-add",
         struct {
             fn f(e: emacs.Env, a: i32, b: i32) emacs.Value {
-                return e.make_integer(a + b);
+                return e.makeInteger(a + b);
             }
         }.f,
+        .{},
     );
 
     return 0;
--- src/root.zig
+++ src/root.zig
@@ -4,65 +4,69 @@ const c = @cImport({
 });
 const root = @import("root");
 
+/// This exported variable and func are required by emacs.
 export const plugin_is_GPL_compatible: c_int = 1;
+export fn emacs_module_init(ert: ?*c.struct_emacs_runtime) callconv(.C) c_int {
+    if (!@hasDecl(root, "init")) @compileError("emacs dynamic module must provider function `init`");
+    if (!@hasDecl(root, "allocator")) @compileError("emacs dynamic module must provide an allocator");
+
+    return root.init(Env.init(
+        ert.?.get_environment.?(ert).?,
+        root.allocator,
+    ));
+}
+
 pub const Value = c.emacs_value;
 const ptrdiff_t = c_long;
 const intmax_t = c_long;
 
-// args: env, nargs: ptrdiff_t, fn: [*c]Value, data:?*anyopaque
-const EmacsFunction = c.emacs_function;
-
-export fn emacs_module_init(ert: ?*c.struct_emacs_runtime) callconv(.C) c_int {
-    if (!@hasDecl(root, "init")) @compileError("emacs dynamic module must contains `fn init(env) c_int` entrypoint");
-
-    const rt = ert.?;
-    return root.init(Env.init(rt.get_environment.?(ert).?));
-}
-
 pub const Env = struct {
     inner: *c.emacs_env,
-
-    make_function: *const fn (
-        [*c]c.emacs_env,
-        ptrdiff_t, // minimum and maximum number of arguments
-        ptrdiff_t, // `emacs_variadic_function` means `&rest`
-        EmacsFunction,
-        [*c]const u8, // docstring
-        ?*anyopaque, // arbitrary additional data to be passed to func
-    ) callconv(.C) c.emacs_value,
+    allocator: std.mem.Allocator,
 
     // global refs
-    nil: c.emacs_value,
-    t: c.emacs_value,
+    nil: Value,
+    t: Value,
+
+    fn init(env: *c.emacs_env, allocator: std.mem.Allocator) Env {
+        return .{
+            .inner = env,
+            .allocator = allocator,
+            .nil = env.make_global_ref.?(env, env.intern.?(env, "nil")),
+            .t = env.make_global_ref.?(env, env.intern.?(env, "t")),
+        };
+    }
 
     // c type -> emacs
-    pub fn make_string(self: Env, input: [:0]const u8) Value {
+    pub fn makeString(self: Env, input: [:0]const u8) Value {
         return self.inner.make_string.?(self.inner, input, @intCast(input.len));
     }
 
-    pub fn make_integer(self: Env, n: c_long) Value {
+    pub fn makeInteger(self: Env, n: c_long) Value {
         return self.inner.make_integer.?(self.inner, n);
     }
 
-    pub fn make_float(self: Env, n: f64) Value {
+    pub fn makeFloat(self: Env, n: f64) Value {
         return self.inner.make_float.?(self.inner, n);
     }
 
     // emacs -> c type
-    pub fn extract_string(self: Env, allocator: std.mem.Allocator, v: Value) ![:0]const u8 {
+    pub fn extractString(self: Env, allocator: std.mem.Allocator, v: Value) ![:0]const u8 {
         var len: c_long = 0;
+        // pass null as buf to get len
         _ = self.inner.copy_string_contents.?(self.inner, v, null, &len);
-        var buf = try allocator.alloc(u8, @intCast(len));
+        const buf = try allocator.alloc(u8, @intCast(len));
 
-        _ = self.inner.copy_string_contents.?(self.inner, v, &buf, &len);
-        return buf;
+        // fill buf
+        _ = self.inner.copy_string_contents.?(self.inner, v, buf.ptr, &len);
+        return buf[0 .. buf.len - 1 :0];
     }
 
-    pub fn extract_integer(self: Env, v: Value) c_long {
+    pub fn extractInteger(self: Env, v: Value) c_long {
         return self.inner.extract_integer.?(self.inner, v);
     }
 
-    pub fn extract_float(self: Env, v: Value) f64 {
+    pub fn extractFloat(self: Env, v: Value) f64 {
         return self.inner.extract_float.?(self.inner, v);
     }
 
@@ -70,7 +74,7 @@ pub const Env = struct {
         return self.inner.intern.?(self.inner, symbol);
     }
 
-    pub fn is_not_nil(self: Env, v: Value) bool {
+    pub fn isNotNil(self: Env, v: Value) bool {
         return self.inner.is_not_nil.?(v);
     }
 
@@ -88,19 +92,15 @@ pub const Env = struct {
     }
 
     pub fn message(self: Env, input: [:0]const u8) Value {
-        return self.funcall("message", &[_]Value{self.make_string(input)});
+        return self.funcall("message", &[_]Value{self.makeString(input)});
     }
 
-    fn init(env: *c.emacs_env) Env {
-        return .{
-            .inner = env,
-            .make_function = env.make_function.?,
-            .nil = env.make_global_ref.?(env, env.intern.?(env, "nil")),
-            .t = env.make_global_ref.?(env, env.intern.?(env, "t")),
-        };
-    }
-
-    pub fn define_fn(self: @This(), comptime name: [:0]const u8, func: anytype) Value {
+    pub fn defineFunc(
+        self: Env,
+        comptime name: [:0]const u8,
+        func: anytype,
+        ctx: FuncContext,
+    ) Value {
         const fn_info = switch (@typeInfo(@TypeOf(func))) {
             .Fn => |fn_info| fn_info,
             else => @compileError("cannot use func, expecting a function"),
@@ -109,20 +109,35 @@ pub const Env = struct {
         if (fn_info.is_var_args) @compileError("emacs function can't be variadic");
         if (fn_info.params.len == 0) @compileError("emacs function should contains at least one arg");
         if (fn_info.params[0].type != Env) @compileError("emacs function first arg should be Env type");
+        if (fn_info.return_type) |ret_type| {
+            if (ret_type != Value) {
+                switch (@typeInfo(ret_type)) {
+                    .ErrorUnion => |err_union| {
+                        if (err_union.payload != Value) {
+                            @compileError("emacs function should return Value or !Value");
+                        }
+                    },
+                    else => @compileError("emacs function should return Value or !Value"),
+                }
+            }
+        } else {
+            @compileError("emacs function should return Value or !Value");
+        }
 
+        const makeFunction = self.inner.make_function.?;
         // subtract the env argument
         const min_args: ptrdiff_t = @as(ptrdiff_t, @intCast(fn_info.params.len)) - 1;
-        const max_args = min_args; // TODO: support &rest args
-        const emacs_fn = self.make_function(
+        const max_args = min_args; // TODO: now we only support fixed number of args.
+        const emacs_fn = makeFunction(
             self.inner,
             min_args,
             max_args,
             struct {
-                fn emacs_fn(e: ?*c.emacs_env, nargs: ptrdiff_t, emacs_args: [*c]c.emacs_value, data: ?*anyopaque) callconv(.C) c.emacs_value {
-                    _ = data;
+                fn emacs_fn(e: ?*c.emacs_env, nargs: ptrdiff_t, emacs_args: [*c]Value, data: ?*anyopaque) callconv(.C) Value {
                     _ = nargs;
+                    _ = data;
 
-                    const zig_env = Env.init(e.?);
+                    const zig_env = Env.init(e.?, root.allocator);
                     var args: std.meta.ArgsTuple(@TypeOf(func)) = undefined;
                     args[0] = zig_env;
                     comptime var i: usize = 0;
@@ -132,32 +147,58 @@ pub const Env = struct {
                         const arg_ptr = &args[i + 1];
                         const ArgType = arg.type.?;
 
-                        setTypeFromValue(ArgType, zig_env, arg_ptr, emacs_args[i].?);
+                        convertTypeFromEmacs(zig_env.allocator, ArgType, zig_env, arg_ptr, emacs_args[i].?) catch |err| {
+                            std.log.err("convert value failed, func:{s}, err:{any}", .{ name, err });
+                            return zig_env.report_error(
+                                std.fmt.comptimePrint("convert value to zig failed, func:{s}", .{name}),
+                                err,
+                            );
+                        };
                     }
-                    return @call(.auto, func, args);
+
+                    return if (fn_info.return_type.? == Value)
+                        @call(.auto, func, args)
+                    else
+                        @call(.auto, func, args) catch |err| {
+                            std.log.err("call function failed, func:{s}, err:{any}", .{ name, err });
+                            return zig_env.report_error(
+                                std.fmt.comptimePrint("call function failed, func:{s}", .{name}),
+                                err,
+                            );
+                        };
                 }
             }.emacs_fn,
-            null,
+            @ptrCast(ctx.doc_string),
             null,
         );
-        return self.funcall("defalias", &[_]c.emacs_value{
+
+        return self.funcall("defalias", &[_]Value{
             self.intern(name), emacs_fn,
         });
     }
+
+    pub fn report_error(self: Env, comptime tmpl: [:0]const u8, err: anyerror) Value {
+        return self.funcall("error", &[_]Value{ self.makeString(tmpl ++ "err:%s"), self.makeString(@errorName(err)) });
+    }
 };
 
-fn setTypeFromValue(comptime ArgType: type, env: Env, arg: *ArgType, v: Value) void {
+/// This function convert Emacs type arg to Zig type.
+fn convertTypeFromEmacs(allocator: std.mem.Allocator, comptime ArgType: type, env: Env, arg: *ArgType, v: Value) !void {
     switch (@typeInfo(ArgType)) {
-        .Float => arg.* = env.extract_float(v),
-        .Int => arg.* = @intCast(env.extract_integer(v)),
+        .Float => arg.* = env.extractFloat(v),
+        .Int => arg.* = @intCast(env.extractInteger(v)),
 
-        // .Pointer => |ptr| switch (ptr.size) {
-        //     .Slice => switch (ptr.child) {
-        //         u8 => arg.* = sliceFromValue(sqlite_value),
-        //         else => @compileError("cannot use an argument of type " ++ @typeName(ArgType)),
-        //     },
-        //     else => @compileError("cannot use an argument of type " ++ @typeName(ArgType)),
-        // },
+        .Pointer => |ptr| switch (ptr.size) {
+            .Slice => switch (ptr.child) {
+                u8 => arg.* = try env.extractString(allocator, v),
+                else => @compileError("cannot use an argument of type " ++ @typeName(ArgType)),
+            },
+            else => @compileError("cannot use an argument of type " ++ @typeName(ArgType)),
+        },
         else => @compileError("cannot use an argument of type " ++ @typeName(ArgType)),
     }
 }
+
+pub const FuncContext = struct {
+    doc_string: ?[:0]const u8 = null,
+};
