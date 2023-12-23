032e3aca1da132ba04e9529245b1d25b5d3e76b5
jiacai2050 <dev@liujiacai.net>
2023-12-23 23:32:58 +0800

    polish README

--- build.zig
+++ build.zig
@@ -7,15 +7,6 @@ pub fn build(b: *std.Build) void {
         .source_file = .{ .path = "src/root.zig" },
     });
 
-    // const lib_unit_tests = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/root.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_lib_unit_tests.step);
-
     const exe = b.addSharedLibrary(.{
         .name = "example",
         .root_source_file = .{ .path = "example.zig" },
